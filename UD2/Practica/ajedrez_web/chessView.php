e3992edbb6b0711fdd7fdbaae1df0c19ec14802e
Castillo5115 <102173631+Castillo5115@users.noreply.github.com>
2023-10-23 22:21:13 +0200

    Gracias Loko

--- UD2/Practica/ajedrez_web/chessView.php
+++ UD2/Practica/ajedrez_web/chessView.php
@@ -3,11 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/chess_game_style.css">
     <title>Chess PVP</title>
 </head>
 <body>
+    <h1>CHESS PVP</h1>
     <?php
+        function DrawChessGame($board)
+        {
+            echo "<table>";
+            for ($fila=0; $fila < 8; $fila++) { 
+                echo "<tr>";
+                for ($columna=0; $columna < 8; $columna++) { 
+                    echo "<tr></tr>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
 
+        DrawChessGame("Hola");
     ?>
 </body>
 </html>
