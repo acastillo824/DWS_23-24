7175d49f6ba423f247478aede01a79e02d8fcf41
acastillo824 <148398279+acastillo824@users.noreply.github.com>
2024-01-23 17:54:34 +0100

    No se hacer una mierda

---
+++ UD4/ChessAPI/Model/PieceValues.cs
@@ -0,0 +1,12 @@
+namespace ChessAPI.Model
+{
+    public class PieceValues
+    {
+        public const int PawnPieceValue = 1;
+        public const int KnightPieceValue = 3;
+        public const int BishopPieceValue = 3;
+        public const int RookPieceValue = 5;
+        public const int QueenValue = 9;
+        public const int KingPieceValue = int.MaxValue;
+    }
+}
